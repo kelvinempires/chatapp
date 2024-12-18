883d67e574d58b20d952d37442cc2cac3da2337d
kelvinempire <kelvinewurum@gmail.com>
2024-12-18 08:00:08 +0100

    first-final-state

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,6 +13,7 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 const port = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,9 +21,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
-// app.get("/", (req, res) => {
-//   res.send("hello world!");
-// });
+
+app.use(express.static(path.join(__dirname, "/frontEnd/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontEnd", "dist", "index.html"));
+});
 
 server.listen(port, () => {
   connectToMongoDB();
