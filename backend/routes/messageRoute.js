fe7be23057e006d28c33cc77c5268978933a6f1e
kelvinempire <kelvinewurum@gmail.com>
2024-12-25 23:39:30 +0100

    renaming package

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { sendMessage, getMessages } from "../controllers/message.js";
+import { sendMessage, getMessages } from "../controllers/messageModel.js";
 import protectRoute from "../middleware/protectRout.js";
 
 const router = express.Router();
